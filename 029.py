42ab8a447ad2b8c4a59dfd0e581f3d95f79c4216
IzoliDenner <dennerizoli12@gmail.com>
2022-05-20 07:21:21 -0300

    Exercicios resolvidos

---
+++ 029.py
@@ -0,0 +1,11 @@
+# Exercício Python 29: Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h,
+# mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
+
+velocidade = (float(input('Qual a velocidade atual do carro? ')))
+if velocidade >= 80:
+    print('MULTADO! Voce execedeu o limite permitido que é de 80km/h')
+    multa = (velocidade - 80) * 7
+    print('Voce deve pagar uma multa de R$ {:.2f}!.'.format(multa))
+print('Tenha um bom dia !! Dirija com segurança.')
+
+# condição simples, sem utilizar a condição ELSgi
